1bd69b9510df3fa37f0c1114a63e4509b8cb7295
Yousuf Ali <yousuf.pharma@gmail.com>
2022-12-12 16:09:04 -0500

    file added

---
+++ bayes.R
@@ -0,0 +1,83 @@
+#seed for replicability
+set.seed(8675309)
+
+# create a N x k matrix of covariates
+N = 250
+K = 3
+
+covariates = replicate(K, rnorm(n=N))
+colnames(covariates) = c('X1', 'X2', 'X3')
+
+# create the model matrix with intercept
+X = cbind(Intercept=1, covariates)
+
+# create a normally distributed variable that is a function of the covariates
+coefs = c(5, .2, -1.5, .9)
+mu = X %*% coefs
+sigma = 2
+y = rnorm(N, mu, sigma)
+
+# same as
+# y = 5 + .2*X1 - 1.5*X2 + .9*X3 + rnorm(N, mean=0, sd=2)
+
+# Run lm for later comparison; but go ahead and examine now if desired
+modlm = lm(y~., data=data.frame(X[,-1]))
+# summary(modlm)
+
+dat = list(N=N, K=ncol(X), y=y, X=X)
+
+stanmodelcode = "
+data {                      // Data block
+int<lower=1> N;           // Sample size
+int<lower=1> K;           // Dimension of model matrix
+matrix[N, K] X;           // Model Matrix
+vector[N] y;              // Target variable
+}
+
+/* 
+transformed data {          // Transformed data block. Not used presently.
+} 
+*/
+
+parameters {                // Parameters block
+vector[K] beta;           // Coefficient vector
+real<lower=0> sigma;      // Error scale
+}
+
+model {                     // Model block
+vector[N] mu;
+mu = X * beta;            // Creation of linear predictor
+
+// priors
+beta ~ normal(0, 10);
+sigma ~ cauchy(0, 5);     // With sigma bounded at 0, this is half-cauchy
+
+// likelihood
+y ~ normal(mu, sigma);
+}
+
+/*
+generated quantities {      // Generated quantities block. Not used presently.
+}
+*/
+"
+library(rstan)
+
+### Run the model and examine results
+fit = stan(model_code = stanmodelcode,
+           data = dat,
+           iter = 5000,
+           warmup = 2500,
+           thin = 10,
+           chains = 4)
+
+print(fit, pars=c('beta', 'sigma'), digits=3, prob=c(.025,.5,.975))
+
+summary(modlm)
+stan_trace(fit, pars=c('beta[4]'))
+
+
+library(coda)
+betas = extract(fit, pars='beta')$beta
+betas.mcmc = as.mcmc(betas)
+plot(betas.mcmc)
